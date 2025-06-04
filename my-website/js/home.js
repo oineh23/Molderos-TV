7a79adaed5eb3d3a9dfee8f32271d5949bd74f62
oineh23 <twistedmetal.oineh23@gmail.com>
2025-06-04 08:09:14 +0800

    Update home.js

--- my-website/js/home.js
+++ my-website/js/home.js
@@ -262,3 +262,48 @@ init().then(() => {
   fetchPinoyMovies();
 });
 
+// ====== PINOY MOVIES ENHANCED ======
+
+let pinoyPage = 1;
+let pinoyGenre = '';
+
+async function fetchPinoyMoviesPaginated(reset = false) {
+  const url = `${BASE_URL}/discover/movie?api_key=${API_KEY}&with_origin_country=PH&with_original_language=tl&sort_by=popularity.desc&page=${pinoyPage}` +
+              (pinoyGenre ? `&with_genres=${pinoyGenre}` : '');
+
+  const results = await fetchData(url);
+  const container = document.getElementById('pinoy-movie-list');
+
+  if (reset) {
+    container.innerHTML = '';
+    pinoyPage = 1;
+  }
+
+  results.forEach(movie => {
+    if (!movie.poster_path) return;
+    movie.media_type = 'movie';
+    container.appendChild(createCard(movie));
+  });
+}
+
+function setupPinoyControls() {
+  const genreSelect = document.getElementById('pinoy-genre-filter');
+  const loadMoreBtn = document.getElementById('load-more-pinoy');
+
+  genreSelect.addEventListener('change', () => {
+    pinoyGenre = genreSelect.value;
+    pinoyPage = 1;
+    fetchPinoyMoviesPaginated(true);
+  });
+
+  loadMoreBtn.addEventListener('click', () => {
+    pinoyPage++;
+    fetchPinoyMoviesPaginated();
+  });
+}
+
+// Call these after init
+init().then(() => {
+  fetchPinoyMoviesPaginated();
+  setupPinoyControls();
+});
