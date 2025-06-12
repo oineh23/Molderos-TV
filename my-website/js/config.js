4d43bc3d3c2423868a0b6b8c815787bcd60f274c
oineh23 <twistedmetal.oineh23@gmail.com>
2025-06-12 12:41:51 +0800

    Add files via upload

---
+++ my-website/js/config.js
@@ -0,0 +1,13 @@
+export const API_KEY = 'b8c2d0fa80cd79b5d28d9fe2853806bb';
+export const BASE_URL = 'https://api.themoviedb.org/3';
+export const IMG_URL = 'https://image.tmdb.org/t/p/original';
+
+export const genreMap = {
+  28: 'Action',
+  35: 'Comedy',
+  18: 'Drama',
+  10765: 'Sci-Fi',
+  16: 'Anime',
+  80: 'Crime',
+  10749: 'Romance',
+};
