dd0274607bfa97dd261d3fc4680db43b84eceacd
oineh23 <twistedmetal.oineh23@gmail.com>
2025-07-07 04:14:10 +0800

    Update home.js

--- my-website/js/home.js
+++ my-website/js/home.js
@@ -1,5 +1,5 @@
 // ====== CONFIGURATION ======
-const TMDB_API_KEY = 'b8c2d0fa80cd79b5d28d9fe2853806bb';
+const API_KEY = 'b8c2d0fa80cd79b5d28d9fe2853806bb';
 const BASE_URL = 'https://api.themoviedb.org/3';
 const IMG_URL = 'https://image.tmdb.org/t/p/original';
 
