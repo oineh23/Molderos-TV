ab6a783d6042bc33cf575fd8f0a44f22a4b60c2a
oineh23 <twistedmetal.oineh23@gmail.com>
2025-07-08 01:14:30 +0800

    Update app.js

--- my-website/js/app.js
+++ my-website/js/app.js
@@ -1,58 +1,35 @@
-$(document).ready(() => {
-    $('#hamburger-menu').click(() => {
-        $('#hamburger-menu').toggleClass('active')
-        $('#nav-menu').toggleClass('active')
-    })
+const arrows = document.querySelectorAll(".arrow");
+const movieLists = document.querySelectorAll(".movie-list");
 
-    // setting owl carousel
+arrows.forEach((arrow, i) => {
+  const itemNumber = movieLists[i].querySelectorAll("img").length;
+  let clickCounter = 0;
+  arrow.addEventListener("click", () => {
+    const ratio = Math.floor(window.innerWidth / 270);
+    clickCounter++;
+    if (itemNumber - (4 + clickCounter) + (4 - ratio) >= 0) {
+      movieLists[i].style.transform = `translateX(${
+        movieLists[i].computedStyleMap().get("transform")[0].x.value - 300
+      }px)`;
+    } else {
+      movieLists[i].style.transform = "translateX(0)";
+      clickCounter = 0;
+    }
+  });
 
-    let navText = ["<i class='bx bx-chevron-left'></i>", "<i class='bx bx-chevron-right'></i>"]
+  console.log(Math.floor(window.innerWidth / 270));
+});
 
-    $('#hero-carousel').owlCarousel({
-        items: 1,
-        dots: false,
-        loop: true,
-        nav:true,
-        navText: navText,
-        autoplay: true,
-        autoplayHoverPause: true
-    })
+//TOGGLE
 
-    $('#top-movies-slide').owlCarousel({
-        items: 2,
-        dots: false,
-        loop: true,
-        autoplay: true,
-        autoplayHoverPause: true,
-        responsive: {
-            500: {
-                items: 3
-            },
-            1280: {
-                items: 4
-            },
-            1600: {
-                items: 6
-            }
-        }
-    })
+const ball = document.querySelector(".toggle-ball");
+const items = document.querySelectorAll(
+  ".container,.movie-list-title,.navbar-container,.sidebar,.left-menu-icon,.toggle"
+);
 
-    $('.movies-slide').owlCarousel({
-        items: 2,
-        dots: false,
-        nav:true,
-        navText: navText,
-        margin: 15,
-        responsive: {
-            500: {
-                items: 2
-            },
-            1280: {
-                items: 4
-            },
-            1600: {
-                items: 6
-            }
-        }
-    })
-})
+ball.addEventListener("click", () => {
+  items.forEach((item) => {
+    item.classList.toggle("active");
+  });
+  ball.classList.toggle("active");
+});
