a757e18d83a06583a5fdc8c007b6367bcab263af
oineh23 <twistedmetal.oineh23@gmail.com>
2025-07-09 13:42:14 +0800

    Update home.js

--- my-website/js/home.js
+++ my-website/js/home.js
@@ -414,13 +414,3 @@ loadMoreKoreanBtn.addEventListener('click', () => {
 
 // Initial Korean movie load
 loadKoreanMovies();
-
-// Function to open the modal
-function openModal() {
-  document.getElementById("myModal").classList.add("active");
-}
-
-// Function to close the modal
-function closeModal() {
-  document.getElementById("myModal").classList.remove("active");
-}
